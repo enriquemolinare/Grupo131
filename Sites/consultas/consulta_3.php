0d2aca7dbd862ccbebe55b51627197f5dade1bc4
JoseAlvarez123 <jjalvarez@uc.cl>
2021-05-24 23:30:44 -0400

    MAS modificaciones, nunca acabaraaan! ... saludos -Jota

--- Sites/consultas/consulta_3.php
+++ Sites/consultas/consulta_3.php
@@ -7,7 +7,7 @@
 
   $tipo_producto_elegido = $_POST["tipo_elegido"];
   
-  $query = "SELECT tiendas.id, tiendas.nombre, direcciones.dirección, comunas.comuna FROM tiendas, productos_tienda, $tipo_producto_elegido, direcciones, comunas WHERE tiendas.id = productos_tienda.tienda_id AND productos_tienda.producto_id = $tipo_producto_elegido.producto_id AND tiendas.direccion_id = direcciones.id AND direcciones.comuna_id = comunas.id GROUP BY tiendas.id, direcciones.dirección, comunas.comuna ORDER BY tiendas.id ASC;";
+  $query = "SELECT tiendas.id, tiendas.nombre, CONCAT(direcciones.dirección, ", ", comunas.comuna) AS direccion FROM tiendas, productos_tienda, $tipo_producto_elegido, direcciones, comunas WHERE tiendas.id = productos_tienda.tienda_id AND productos_tienda.producto_id = $tipo_producto_elegido.producto_id AND tiendas.direccion_id = direcciones.id AND direcciones.comuna_id = comunas.id GROUP BY tiendas.id, direcciones.dirección, comunas.comuna ORDER BY tiendas.id ASC;";
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$dataCollected = $result -> fetchAll();
@@ -17,10 +17,11 @@
     <tr>
       <th>ID</th>
       <th>Tienda</th>
+      <th>Direccion</th>
     </tr>
   <?php
 	foreach ($dataCollected as $tupla) {
-  		echo "<tr> <td>$tupla[0]</td> <td>$tupla[1]</td> </tr>";
+  		echo "<tr> <td>$tupla[0]</td> <td>$tupla[1]</td> <td>$tupla[2]</td> </tr>";
 	}
   ?>
 	</table>
