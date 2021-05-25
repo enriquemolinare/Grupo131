a418969e4ad32f7d34b4db357aeb85d95f134ac8
JoseAlvarez123 <jjalvarez@uc.cl>
2021-05-24 20:53:18 -0400

    Subo archivo de pruebe en Sites, saludos! -Jota

---
+++ Sites/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http equiv="Content Type" content="text/html; charset=UTF-8" />
+    <title> Ejemplo PHP </title>
+</head>
+
+<body>
+
+<h1> Insertar Titulo aqui!</h1>
+<p>Esto es un parrafo!</p>
+
+<form>
+Ingrese una comuna: <input type="text" name="comuna_ingresada">
+</form> 
+
+<?php
+
+#variable
+$variable = 10;
+$texto = "Hola cabros";
+
+#mostrar en la vista:
+echo "Esto se esta imprimiendo desde PHP, mensaje: $texto";
+
+
+
+
+
+
+?>
+
+
+</body>
+</html>
