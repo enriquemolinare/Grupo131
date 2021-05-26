ebb2e32db6bc43986f727937888ca9a5e9c2976a
JoseAlvarez123 <jjalvarez@uc.cl>
2021-05-26 16:51:13 -0400

    Saludos. -Jota

--- Sites/index.php
+++ Sites/index.php
@@ -27,12 +27,12 @@
 <h3 align="center"> Consulta 3: ¿Quieres saber las tiendas que venden un cierto tipo de producto? </h3>
 <form align="center" action="consultas/consulta_3.php" method="post">
     Ingrese tipo producto (no comestibles, frescos, congelados, conservas):
-    <select>
-      <option name="tipo_elegido" value="no comestibles">No comestibles</option>
-      <option name="tipo_elegido" value="comestibles">Comestibles</option>
-      <option name="tipo_elegido" value="comestibles frescos">Comestibles frescos</option>
-      <option name="tipo_elegido" value="comestibles congelados">Comestibles congelados</option>
-      <option name="tipo_elegido" value="comestibles en conserva">Comestibles en conserva</option>
+    <select name="tipo_elegido" >
+      <option value="no comestibles">No comestibles</option>
+      <option value="comestibles">Comestibles</option>
+      <option value="comestibles frescos">Comestibles frescos</option>
+      <option value="comestibles congelados">Comestibles congelados</option>
+      <option value="comestibles en conserva">Comestibles en conserva</option>
     </select>
     <br/><br/>
     <input type="submit" value="Buscar">
